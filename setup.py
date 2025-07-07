d1097064450c8162ea425fb700080aa2e93c7c89
Nutnell <nutnell00@gmail.com>
2025-07-07 15:29:23 +0300

    File updated

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-""
+from setuptools import find_packages,setup
+
+setup(
+    name='MCQGenrator',
+    version='0.0.1',
+    author='Nutnell',
+    author_email='nutnell00@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","Pypdf"],
+    packages=find_packages()
+)
